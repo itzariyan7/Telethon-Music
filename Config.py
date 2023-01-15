e144e7531a5af7b5bb485edef7268ff4ee5d18cb
Itz Ariyan <122726541+itzariyan7@users.noreply.github.com>
2023-01-16 02:35:04 +0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,15 +3,15 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6435225"))
     API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5896516651:AAHfMaFJM71cML5_1owz_OR_8bVmWZqSzQg")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOIEBu1LR_Yd_glurY1W7nAZnKDMLCGzFdRMsUAEHX0lJOCEdlTfYTZ3mPIy784r8NEXIKrfDOj9YvlW7KQBIAnrRhEgOJ6bspdKR8RQYyLA5lcznFEfiLSJzcGVPus8ruar1hJiHK2qn-t6qp5OX6dd-QSqymWY28DHk5OqQ4XCZSiFMnbCb1eZVMNGPzsuvIiCaBv3yiwG-uPehqhMG_JGLsp8lHx7pwo6jQWcCd8boD0IOjWXUIFpRfDwVg_fk5CszZUfH064k0Msum2n77vNx2139odhzhr4B9jOTh9xmEZ9bcSceOX1JnPSJxSNwFenw5AYwhBhLGeSi8fMTqrUdi-I=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "NixaVcBot")
+    SUPPORT = os.environ.get("SUPPORT", "SankiWorldMF") # Your Support
+    CHANNEL = os.environ.get("CHANNEL", "NixaWorld") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5688281798")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
-    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
+    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', Truest) # Change it to "True"
